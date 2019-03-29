32ce2765976ee880f1e668377f6cecce848d0c9b
Lee-Junhee <jlee02@stuy.edu>
2019-03-28 21:36:38 -0400

    created list of methods

---
+++ OrderedList_inArraySlots.java
@@ -0,0 +1,7 @@
+public class OrderedList_inArraySlots {
+    List_inArraySlots list;
+
+    public OrderedList_inArraySlots() {
+        list = new List_inArraySlots();
+    }
+}
---
+++ UserOfOrderedList.java
@@ -0,0 +1,28 @@
+public class UserOfOrderedList {
+    public static void main(String[] args) {
+        //initialize the list, test no arg constructor
+	OrderedList_inArraySlots orderedList = new OrderedList_inArraySlots();
+
+        // //test the add(), required to populate the list
+	// orderedList.add( "Stuy");
+	// orderedList.add( 42);
+        // orderedList.add( 2.718);
+
+	// //test the toString(), verify that add() worked properly
+	// System.out.println(orderedList);
+
+        // //test the size() method
+	// System.out.println( "size of ordered list: "
+	// 		  + orderedList.size());
+
+	// //test the get() method
+	// System.out.println( orderedList.get( 1));
+
+	// //set() seems like it doesn't apply to ordered lists
+	// 
+	// //test the remove() method
+	// orderedList.remove( 1);
+	// System.out.println( orderedList);
+    }
+}
+
